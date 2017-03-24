d2d16e7724375442e1a7d64ea1ecbaead5ddce80
Wei <cloud9@weispot.com>
2017-03-24 19:33:01 +0000

    Updated Background

--- src/components/templates/PageTemplate/index.js
+++ src/components/templates/PageTemplate/index.js
@@ -6,7 +6,9 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: center;
   min-height: 100vh;
-  background: #57637a url("images/bg-home.jpg") no-repeat center;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0), #293c66 0.1%, #2e446d 4.6%, #556d99 26.8%, #62789f 34.2%, #667a9f 35.8%, #707b9b 43.6%, #6e7c99 43.6%, #6d7792 48.1%, #6f7692 48.1%, #33333b 90.2%, #323338 90.2%, #2b2c31) no-repeat center;
+  background-color: #57637a;
+  background-image: url("images/bg-home.jpg");
   background-size: cover;
   
   @media only screen and (max-width: 500px) {
