a4a4e4f5df9056d966ad504234d3d4ef55be89a1
Wei <cloud9@weispot.com>
2017-08-04 19:44:59 +0000

    Added Google Analytics and Removed Scripts from Page

--- src/components/Html.js
+++ src/components/Html.js
@@ -2,6 +2,16 @@
 import React, { PropTypes } from 'react'
 import Helmet from 'react-helmet'
 
+const gaCode = `
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-18952818-14', 'auto');
+ga('send', 'pageview');
+`
+
 const Html = ({ styles, assets, state, content }) => {
   const helmet = Helmet.rewind()
   const attrs = helmet.htmlAttributes.toComponent()
@@ -18,10 +28,13 @@ const Html = ({ styles, assets, state, content }) => {
       <body style={{ display: 'none' }}>
         <a href="https://blog.whe.me/" style={{ display: 'none' }}>Wei&apos;s Blog</a>
         <main id="app" dangerouslySetInnerHTML={{ __html: content }} />
+        {/*
         <script dangerouslySetInnerHTML={{ __html: state }} />
         {Object.keys(assets.javascript).reverse().map((key) =>
           <script key={key} src={assets.javascript[key]} />
         )}
+        */}
+        <script dangerouslySetInnerHTML={{ __html: gaCode }} />
       </body>
     </html>
   )
